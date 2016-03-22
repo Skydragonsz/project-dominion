4c5f4a401fe05acc69a5c60b614861ab47b52d52
Arthur Saprunov <Arthur.saprunov@student.howest.be>
2016-03-22 14:00:10 +0100

    Cards test

---
+++ Dominion/src/Cards/ActionCards.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Cards;
+
+/**
+ *
+ * @author Arthur
+ */
+public class ActionCards {
+    private static final String TYPE= "ACTION";
+    
+    
+    public String getType(){
+        return TYPE;
+    }
+}
---
+++ Dominion/test/Tester/CardsTester.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tester;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import Cards.*;
+
+/**
+ *
+ * @author Arthur
+ */
+public class CardsTester {
+    private Witch w = new Witch();
+    public CardsTester() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+    @Test
+    public void testGetType(){
+        System.out.println(w.getType());
+    }
+}
