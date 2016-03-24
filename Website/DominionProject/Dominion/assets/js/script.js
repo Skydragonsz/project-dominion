6e54160d09b707afc3a45851e70bf5ba0687b967
GianniCoene <gianni.coene@student.howest.be>
2016-03-24 16:16:16 +0100

    website v1.0.2.1
    Card hover moet nog aangepast worden

--- Website/DominionProject/Dominion/assets/js/script.js
+++ Website/DominionProject/Dominion/assets/js/script.js
@@ -1,3 +1,21 @@
 /**
  * Created by Gianni on 3/22/2016.
  */
+
+var changeImage = function() {
+    $("img")
+        .mouseover(function() {
+            var src = $(this).attr("src").replace("_small", "");
+            $(this).attr("src", src);
+        })
+        .mouseout(function() {
+            var src = $(this).attr("src").replace(".jpg", "_small.jpg");
+            $(this).attr("src", src);
+        });
+};
+
+
+
+$(document).ready(function(){
+    $('img').on('mouseover', changeImage());
+});
