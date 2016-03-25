cb1aabbf2f007d19d85a874978b424c5247f38db
GianniCoene <gianni.coene@student.howest.be>
2016-03-25 11:19:30 +0100

    website v1.0.2.2
    Fixed cards en scrolling animation

--- Website/DominionProject/Dominion/assets/js/script.js
+++ Website/DominionProject/Dominion/assets/js/script.js
@@ -2,6 +2,7 @@
  * Created by Gianni on 3/22/2016.
  */
 
+/*
 var changeImage = function() {
     $("img")
         .mouseover(function() {
@@ -18,4 +19,20 @@ var changeImage = function() {
 
 $(document).ready(function(){
     $('img').on('mouseover', changeImage());
+});
+*/
+
+$(document).ready(function(){
+    $('a[href^="#"]').on('click',function (e) {
+        e.preventDefault();
+
+        var target = this.hash;
+        var $target = $(target);
+
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 900, 'swing', function () {
+            window.location.hash = target;
+        });
+    });
 });
