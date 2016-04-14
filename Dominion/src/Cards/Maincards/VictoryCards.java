e51594a8762fc10305b4864152f1730f96c9bc42
Arthur Saprunov <Arthur.saprunov@student.howest.be>
2016-04-14 11:40:43 +0200

    Some commits didn't push!
    Added the cards that didn't were included in the other commit

---
+++ Dominion/src/Cards/Maincards/VictoryCards.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cards.Maincards;
+
+import cards.Maincards.MainCard;
+
+/**
+ *
+ * @author Arthur
+ */
+public class VictoryCards extends MainCard{
+    private final String TYPE= "VICTORY";
+
+    
+    @Override
+    public String getCardType(){
+        return TYPE;
+    }
+    
+
+}
