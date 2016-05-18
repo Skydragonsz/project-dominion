e01aae78e56970eecdd3eae9c93a58c90fd8bd2e
Arthur Saprunov <arthur.saprunov@student.howest.be>
2016-05-18 21:42:51 +0200

    PHP test added

---
+++ html-frontend/src/test/Hello.java
@@ -0,0 +1,61 @@
+package test;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import php.java.servlet.RemoteHttpServletContextFactory;
+
+/**
+ * Servlet implementation class Hello
+ */
+@WebServlet("/Hello")
+public class Hello extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Hello() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.getWriter().append("\n");
+		response.getWriter().write("HelloServlet is running!");
+	}
+
+	/**
+	 * @see HttpServlet#doPut(HttpServletRequest, HttpServletResponse)
+	 */
+	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		RemoteHttpServletContextFactory ctx = new RemoteHttpServletContextFactory(this, 
+				  getServletContext(), request, request, response);
+
+				response.setHeader("X_JAVABRIDGE_CONTEXT", ctx.getId());
+				response.setHeader("Pragma", "no-cache");
+				response.setHeader("Cache-Control", "no-cache");
+
+				try { 
+				  ctx.handleRequests(request.getInputStream(), response.getOutputStream()); 
+				} finally { 
+				  ctx.destroy(); 
+				}
+	}
+	
+	public String hello() {
+		return "hello from MyServlet";
+	}
+
+}
