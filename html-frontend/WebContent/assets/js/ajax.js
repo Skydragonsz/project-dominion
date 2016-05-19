bcf49c47d31f28333f49f263470c0ce667ad5fbd
GianniCoene <gianni.coene@student.howest.be>
2016-05-19 15:49:23 +0200

    Changes to images and ajax call for cards

--- html-frontend/WebContent/assets/js/ajax.js
+++ html-frontend/WebContent/assets/js/ajax.js
@@ -4,20 +4,36 @@
 $(document).ready(function () {
     $("#test").click(function () {
 
-        var request = $.ajax({ cache: false,
+         $.ajax({ cache: false,
             dataType: "text",
             url: "/html-frontend/DominionServlet",
+             type: "get",
             data: {
                 operation: 'initialize',
                 playerAmount: '2',
                 name1: 'Quint',
                 name2: 'Coene'
             }
-        });
-        request.done(function (data) {
-            var obj = JSON.stringify(data);
 
-            console.log("Operation: " + obj.operation + "playerAmount: " + obj.playerAmount + " name1 " + obj.name1 + " name2 " + obj.name2);
+         }).done(function (data) {
+            var obj = JSON.parse(data);
+
+           console.log("Operation: " + obj.operation + " playerAmount: " + obj.playerAmount + " name1 " + obj.name1 + " name2 " + obj.name2);
         });
     });
+
+
 });
+
+
+function callHand() {
+
+$.get("DominionServlet?operation=getHand", function(responseJson) {    // Execute Ajax GET request on URL of "someservlet" and execute the following function with Ajax response JSON... // Create HTML <ul> element and append it to HTML DOM element with ID "somediv".
+    $.each(responseJson, function(index, item) { // Iterate over the JSON array.
+        $("#hand").append('<div class="hand card" style="position: relative; background-image: url(../html-frontend/assets/media/Base%20Deck/'+ item.toLowerCase() +'.jpg)"></div>');
+        //$("#hand").append('<img src="../html-frontend/assets/media/Base%20Deck/'+ item.toLowerCase() +'.jpg">');
+    })
+});
+
+}
+
--- html-frontend/WebContent/assets/js/script.js
+++ html-frontend/WebContent/assets/js/script.js
@@ -13,18 +13,23 @@ $(document).ready(function () {
 	
 	$("img").click(function() {
 		
-		var request = $.ajax({ cache: false,
-			dataType: "text",
-		    url: "/html-frontend/DominionServlet",
-		    data: { operation: 'initialize',
-		    	name1: $("h1").val(),
-		    	name2: $("h2").val() }
-		});
-		request.done(function (data) {
-			var obj = JSON.stringify(data);
-			
-		    console.log("pls");
-		});
+//		var request = $.ajax({ cache: false,
+//			dataType: "text",
+//		    url: "/html-frontend/DominionServlet",
+//		    data: { operation: 'initialize',
+//		    	name1: $("h1").val(),
+//		    	name2: $("h2").val() }
+//		});
+//		request.done(function (data) {
+//			var obj = JSON.stringify(data);
+//			
+//		    console.log("pls");
+//		});
+		$.get("DominionServlet", function(responseJson) {    // Execute Ajax GET request on URL of "someservlet" and execute the following function with Ajax response JSON... // Create HTML <ul> element and append it to HTML DOM element with ID "somediv".
+	        $.each(responseJson, function(index, item) { // Iterate over the JSON array.
+	           console.log(item);      // Create HTML <li> element, set its text content with currently iterated item and append it to the <ul>.
+	        })
+	        });
 		});
 
 
