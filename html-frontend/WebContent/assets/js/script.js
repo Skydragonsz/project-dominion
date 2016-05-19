f7ee9ce30adfeac43fbfa7c4e035b1f4543052ea
QuintenDegraeve <Quinten.Degraeve@student.howest.be>
2016-05-19 13:42:56 +0200

    Servlet init game
    For front-end use.

--- html-frontend/WebContent/assets/js/script.js
+++ html-frontend/WebContent/assets/js/script.js
@@ -11,10 +11,16 @@ $(document).ready(function () {
     calculateDegreesCardsInHand();
     */
 
-    $.get("DominionServlet", function (responseText) {   // Execute Ajax GET request on URL of "someservlet" and execute the following function with Ajax response text...
-        console.log(responseText);
-    });
+//    $.get("DominionServlet", function (responseText) {   // Execute Ajax GET request on URL of "someservlet" and execute the following function with Ajax response text...
+//        console.log(responseText);
+//    });
 
+	$.get("DominionServlet", function(responseJson) {    // Execute Ajax GET request on URL of "someservlet" and execute the following function with Ajax response JSON... // Create HTML <ul> element and append it to HTML DOM element with ID "somediv".
+        $.each(responseJson, function(index, item) { // Iterate over the JSON array.
+           console.log(item);      // Create HTML <li> element, set its text content with currently iterated item and append it to the <ul>.
+        })
+        });
+	
 
     $(".hand").on("click", function () {
         $(this).detach().appendTo("#playingfield");
