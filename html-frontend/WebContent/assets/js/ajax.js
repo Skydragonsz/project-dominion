ea53d12267acb3fdf954cbe888ebcea9c99323cc
Arthur Saprunov <arthur.saprunov@student.howest.be>
2016-05-24 20:12:57 +0200

    Small push/code moved

--- html-frontend/WebContent/assets/js/ajax.js
+++ html-frontend/WebContent/assets/js/ajax.js
@@ -93,16 +93,7 @@ function callPlayerInfo() {
     })
 }
 
-$('.btn-click').click(function () { 
-        $.get('Load', function (responseJson) {    // Execute Ajax GET request on URL of "someservlet" and execute the following function with Ajax response JSON...
-            alert(responseJson);
-            var $select = $('#saves');                           // Locate HTML DOM element with ID "someselect".
-            $select.find('option').remove();                          // Find all child elements with tag name "option" and remove them (just to prevent duplicate options when button is pressed again).
-            $.each(responseJson, function (key, value) {               // Iterate over the JSON object.
-                $('<option>').val(key).text(value).appendTo($select); // Create HTML <option> element, set its value with currently iterated key and its text content with currently iterated item and finally append it to the <select>.
-            });
-        });
-});
+
 
 
 /* All cards */
